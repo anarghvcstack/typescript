658c6b66765bf300e781579ba7c9aec70529cb24
Anargh V <anarghv@carestack.com>
2022-10-14 12:26:21 +0530

    First app

---
+++ index.ts
@@ -0,0 +1,41 @@
+interface Coordinates {
+  x: number;
+  y: number;
+}
+
+interface IMap {
+  location: string;
+  coordinates: Coordinates
+}
+
+class MapClass {
+  location: string;
+  coordinates: Coordinates;
+}
+
+let myMap: Array<IMap> = [
+  {
+    location: 'Trivandrum',
+    coordinates: {
+      x: 1,
+      y: 2
+    }
+  },
+  {
+    location: 'Kochi',
+    coordinates: {
+      x: 1,
+      y: 2
+    }
+  }
+]
+
+let myMap2: MapClass = {
+  location: 'Trivandrum',
+  coordinates: {
+    x: 1,
+    y: 2
+  }
+}
+
+console.log('New change');
